a39ecd11fa4b36527a9812160048c8b6c3367032
gmorong <146980143+gmorong@users.noreply.github.com>
2024-04-11 17:48:11 +0500

    main + lr2
    фикс иерархия
    пулл лр2

---
+++ Сетевое_программирование/Практические_работы/ПрР2/kamkov-rest-api/dbConnection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+const db_connection = mysql
+    .createConnection({
+    host: "localhost", // имя хоста
+    user: "root", // имя пользователя
+    database: "pe-12b_kamkov", // имя базы данных
+    password: "root", // пароль от базы данных
+    })
+
+    .on("error", (err) => {
+        console.log("Failed to connect to Database - ", err);
+});
+
+module.exports = db_connection;
