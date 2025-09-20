4bc4be42ae1bfc781264b8857069c8a48efd0350
Dmitry_PE-12n <dimon.kamkov@mail.ru>
2025-09-20 20:22:11 +0300

    update
    Удалил оставшиеся файлы для учебы

--- Сетевое_программирование/4_Практические_работы/ПрР3/utils/dbConnection.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const dbConnection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'kamkov_login'
-});
-module.exports = dbConnection.promise();
